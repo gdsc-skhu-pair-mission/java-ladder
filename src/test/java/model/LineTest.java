b5a1dde71a21755e12d9e90c6c7b3122c00c3768
yeeun0702 <kimyeeun0702@naver.com>
2024-05-06 13:51:03 +0900

    test: 사다리 가로라인에서 True가 겹치면 재조정하는 메소드 테스트코드 구현

---
+++ src/test/java/model/LineTest.java
@@ -0,0 +1,27 @@
+package model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import java.util.Arrays;
+import java.util.List;
+
+class LineTest {
+    @Test
+    @DisplayName("사다리 가로라인(Line)을 겹치지 않게 생성하는 메소드 테스트")
+    void should_ReraangeLadderLine_When_DuplicateTrue() {
+        // given
+        Line line = new Line(6); // 사람 수를 6로 설정, 한 행에 세로 선이 얼마나 있는지
+        List<Boolean> points = Arrays.asList(true, true, true, true, true);
+
+        // when
+        line.rearrange(points);
+
+        // then
+        for (int i = 0; i < points.size() - 1; i++) {
+            assertFalse(points.get(i) && points.get(i + 1)); // 연속된 true가 없어야 함
+        }
+    }
+}
