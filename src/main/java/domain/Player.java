cb73001ec75f2b532eacfbb8f14e7dc4cdfae90b
bbggr1209 <rsd3010@office.skhu.ac.kr>
2024-05-08 14:11:45 +0900

    플레이어 코드작성

--- src/main/java/domain/Player.java
+++ src/main/java/domain/Player.java
@@ -1,5 +1,13 @@
 package domain;
 
 public class Player {
+    private final String playerName;
 
+    public Player(String playerName) {
+        this.playerName = playerName;
+    }
+
+    public String getPlayerName() {
+        return playerName;
+    }
 }
