df1619c742c78b3182b2ba3cee219c614562fdcc
Park, Sojeong <sojeong2101@gmail.com>
2024-05-08 20:33:25 +0900

    test: 입력 받은 이름 재가공 메소드 테스트 구현

--- src/test/java/model/PersonListTest.java
+++ src/test/java/model/PersonListTest.java
@@ -5,7 +5,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class PersonListTest {
+class PersonListTest {
 
     @Test
     @DisplayName("입력 받은 이름을 쉼표 기준으로 분리시켜 PersonList에 넣기")
@@ -23,4 +23,19 @@ public class PersonListTest {
                 .contains("소정", "예은", "GDSC");
     }
 
+    @Test
+    @DisplayName("입력 받은 이름의 길이에 따라 형식에 맞추어 재가공하기")
+    void should_ManipulateNames_When_InputNames() {
+        // given
+        String peopleName = "gdsc,소정,예은,water,a";
+        PersonList personList = new PersonList(peopleName);
+        personList.personNameInput();
+
+        // when
+        personList.manipulateNames();
+
+        // then
+        assertThat(personList.getManipulationPeopleNames())
+                .contains(" gdsc ", "   소정 ", "   예은 ", " water", "    a ");
+    }
 }
