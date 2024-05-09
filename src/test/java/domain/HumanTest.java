9c9dc58e014a7936967b5403591ad14f1f9bb760
Junyeong-An <2117ab@gmail.com>
2024-05-09 19:00:52 +0900

    feat: 이름 생성 test

---
+++ src/test/java/domain/HumanTest.java
@@ -0,0 +1,26 @@
+package domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+
+public class HumanTest {
+    @Test
+    @DisplayName("사람 이름은 5글자까지 부여하면 생성된다.")
+    void createName(){
+        //given
+        String validName = "pobiaa";
+        assertThatThrownBy(()-> new Human("pobiaa"))
+                .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessage("이름의 길이는 5글자 이하여야 합니다.");
+    }
+
+
+    //when
+
+
+    //then
+}
