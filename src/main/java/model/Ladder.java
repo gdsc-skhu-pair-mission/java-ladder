468d016f5003e71e96619870e94669a19836a5d4
Yehyeok Bang <qkddpgur318@gmail.com>
2024-05-10 12:09:59 +0900

    feat: 사다리 구현

--- src/main/java/model/Ladder.java
+++ src/main/java/model/Ladder.java
@@ -1,4 +1,23 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Ladder {
+    private final GenerateStrategy generateStrategy;
+    private final List<Row> rows = new ArrayList<>();
+
+    public Ladder(GenerateStrategy generateStrategy) {
+        this.generateStrategy = generateStrategy;
+    }
+
+    public void init(int countOfPlayer, int countOfHeight) {
+        for (int i = 0; i < countOfHeight; i++) {
+            rows.add(new Row(countOfPlayer, generateStrategy));
+        }
+    }
+
+    public int getRowSize() {
+        return rows.size();
+    }
 }
