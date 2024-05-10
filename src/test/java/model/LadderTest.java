0393adacf69504fabcb633a586c19311fdef0301
Yehyeok Bang <qkddpgur318@gmail.com>
2024-05-10 12:06:20 +0900

    test: 사다리 관련 테스트 작성

---
+++ src/test/java/model/LadderTest.java
@@ -0,0 +1,39 @@
+package model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+class LadderTest {
+    @Test
+    @DisplayName("정상적으로 사다리 객체 생성")
+    void should_CreateLadderObjectSuccessfully() {
+        // given
+        GenerateStrategy generateStrategy = new RandomGenerateStrategy();
+
+        // when
+        Ladder ladder = new Ladder(generateStrategy);
+
+        // then
+        assertNotNull(ladder);
+    }
+
+    @Test
+    @DisplayName("정상적으로 사다리 초기화")
+    void should_InitializeLadderSuccessfully() {
+        // given
+        GenerateStrategy generateStrategy = new RandomGenerateStrategy();
+        int countOfPlayer = 4;
+        int countOfHeight = 5;
+
+        // when
+        Ladder ladder = new Ladder(generateStrategy);
+        ladder.init(countOfPlayer, countOfHeight);
+
+        // then
+        assertNotNull(ladder);
+        assertEquals(countOfHeight, ladder.getRowSize());
+    }
+}
