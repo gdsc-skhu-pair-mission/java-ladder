b2a18b94f01f0490345b1addc8a133309ef04b10
Junyeong-An <2117ab@gmail.com>
2024-05-12 15:55:24 +0900

    feat: Test 초기 생성

---
+++ src/test/java/view/OutputViewTest.java
@@ -0,0 +1,20 @@
+package view;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class OutputViewTest {
+    @Test
+    @DisplayName("사람 출력")
+    void 사람이름_출력하기(){
+        OutputView outputView = new OutputView();
+        String[] personnal = {"pobi", "honux", "crong", "jk"};
+        String result = "pobi honux crong jk ";
+        assertEquals(result, outputView.getClass(personnal));
+    }
+
+    }
+
+}
