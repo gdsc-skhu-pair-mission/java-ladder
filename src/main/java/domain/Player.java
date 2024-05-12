a5bf955c2da2ddf108ca656224753be5a2fc5369
bbggr1209 <rsd3010@office.skhu.ac.kr>
2024-05-12 21:30:52 +0900

    feat: 플레이어 구현

--- src/main/java/domain/Player.java
+++ src/main/java/domain/Player.java
@@ -1,13 +1,37 @@
 package domain;
 
 public class Player {
-    private final String playerName;
+    private final static int MAX_NAME_LENGTH = 5;
 
-    public Player(String playerName) {
-        this.playerName = playerName;
+    private final String name;
+
+    public Player(String name) {
+        validateNameNull(name);
+        validateNameEmpty(name);
+        validateNameLength(name);
+        this.name = name;
     }
 
-    public String getPlayerName() {
-        return playerName;
+    public String getName() {
+        return name;
     }
+
+    public void validateNameNull(String name) {
+        if (name == null) {
+            throw new IllegalArgumentException("이름은 null이 될 수 없습니다.");
+        }
+    }
+
+    public void validateNameEmpty(String name) {
+        if (name.isEmpty()) {
+            throw new IllegalArgumentException("이름은 빈 문자열이 될 수 없습니다.");
+        }
+    }
+
+    public void validateNameLength(String name) {
+        if (name.length() > MAX_NAME_LENGTH) {
+            throw new IllegalArgumentException("이름 길이를 초과할 수 없습니다.");
+        }
+    }
+
 }
