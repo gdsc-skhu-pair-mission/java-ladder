04bff3cc3c7b09e2ac85100c8db5def9db6d7343
Junyeong-An <2117ab@gmail.com>
2024-05-12 18:32:38 +0900

    fix: view 수정

--- src/test/java/view/OutputViewTest.java
+++ src/test/java/view/OutputViewTest.java
@@ -1,11 +1,28 @@
 package view;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class OutputViewTest {
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+
+    @BeforeEach
+    void setUp(){
+        System.setOut(new PrintStream(outContent));
+    }
+    @AfterEach
+    void tearDown(){
+        System.setOut(originalOut);
+    }
+
     @Test
     @DisplayName("사람 이름 출력")
     void printPersonName(){
