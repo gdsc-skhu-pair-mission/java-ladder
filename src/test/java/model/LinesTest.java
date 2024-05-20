f48250c3623ea9c89dc8f082cefe3cd6d594b9b8
woogym <kwj9294@gmail.com>
2024-05-21 01:04:20 +0900

    feature: LinesTest addLineTest method

--- src/test/java/model/LinesTest.java
+++ src/test/java/model/LinesTest.java
@@ -1,7 +1,28 @@
 package model;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import util.FootholdGenerator;
+import util.TestFootholdGenerator;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class LinesTest {
 
+    @Test
+    @DisplayName("addLine 메소드가 정상적으로 작동하는지 테스트 합니다.")
+    void addLineTest() {
+        FootholdGenerator footholdGenerator = new TestFootholdGenerator(true);
+
+        Line line1 = new Line(footholdGenerator, 5);
+        line1.makeLine();
+        Line line2 = new Line(footholdGenerator, 5);
+        line2.makeLine();
+
+        Lines lines = new Lines();
+        lines.addLine(line1);
+        lines.addLine(line2);
+
+        assertEquals(2, lines.getLines().size());
+    }
 }
