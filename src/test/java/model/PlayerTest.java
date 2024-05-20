44bdede4177584d8b56ef321692e0a7092931cb4
woogym <kwj9294@gmail.com>
2024-05-21 01:13:40 +0900

    style: modify overall test code method naming

--- src/test/java/model/PlayerTest.java
+++ src/test/java/model/PlayerTest.java
@@ -9,8 +9,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class PlayerTest {
 
     @Test
-    @DisplayName("정상적으로 참여자 객체 생성")
-    void Test_GetName() {
+    @DisplayName("정상적으로 참여자 객체 생성되는지 테스트합니다")
+    void createPlayerObjectTest() {
         Player player = new Player("pobix");
 
         assertEquals("pobix", player.getName());
