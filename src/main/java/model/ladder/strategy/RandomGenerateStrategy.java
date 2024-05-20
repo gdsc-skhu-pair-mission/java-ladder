58bd1559f954271504d92d721d3e0618afe4e21c
dongkyunKim <kyun010713@gmail.com>
2024-05-20 13:20:30 +0900

    refactor: 들여쓰기가 1이 되도록 수정

--- src/main/java/model/ladder/strategy/RandomGenerateStrategy.java
+++ src/main/java/model/ladder/strategy/RandomGenerateStrategy.java
@@ -5,23 +5,33 @@ import java.util.List;
 
 public class RandomGenerateStrategy implements GenerateStrategy {
     private static final int BRIDGE_UNCONNECTED = 0;
+    private static final int MINIMUM_PLAYER_COUNT = 2;
 
     @Override
     public List<Boolean> generate(int countOfPlayer) {
-        if (countOfPlayer < 2) {
+        validatePlayerCount(countOfPlayer);
+        return createBridges(countOfPlayer - 1);
+    }
+
+    private void validatePlayerCount(int countOfPlayer) {
+        if (countOfPlayer < MINIMUM_PLAYER_COUNT) {
             throw new IllegalArgumentException("플레이어는 최소 2명 이상이어야 합니다.");
         }
-        int countOfBridge = countOfPlayer - 1;
+    }
 
+    private List<Boolean> createBridges(int countOfBridge) {
         List<Boolean> bridges = new ArrayList<>();
         for (int i = 0; i < countOfBridge; i++) {
-            if (wasPreviousBridgeConnected(bridges, i)) {
-                bridges.add(false);
-                continue;
-            }
-            bridges.add(decideBridgeConnection());
+            bridges.add(decideBridgeConnection(bridges, i));
         }
         return bridges;
+    }
+
+    private boolean decideBridgeConnection(List<Boolean> bridges, int currentIndex) {
+        if (wasPreviousBridgeConnected(bridges, currentIndex)) {
+            return false;
+        }
+        return decideBridgeConnection();
     }
 
     private static boolean wasPreviousBridgeConnected(List<Boolean> bridges, int currentIndex) {
