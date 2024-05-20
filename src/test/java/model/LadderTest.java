01000e6aca99aae359ed5a713f86e63b24b9920d
woogym <kwj9294@gmail.com>
2024-05-21 00:43:32 +0900

    fix: modify generateLadder method test

--- src/test/java/model/LadderTest.java
+++ src/test/java/model/LadderTest.java
@@ -11,13 +11,11 @@ import java.util.Arrays;
 class LadderTest {
 
     private Ladder ladder;
-    private Players players;
     private Height height;
 
     @BeforeEach
     void setUp() {
         ladder = new Ladder();
-        players = new Players(Arrays.asList("a", "bb", "ccc", "dddd", "eeeee"));
         height = new Height("5");
     }
 
@@ -25,9 +23,13 @@ class LadderTest {
     @DisplayName("generateLadder 메소드가 정상적으로 작동하는지 테스트 합니다.")
     void TEST_generateLadder_OPERATION() {
 
-        String ladderResult = ladder.generateLadder(height, players);
-        String[] lines = ladderResult.split("\n");
+        int size = 5;
+        Lines lines = ladder.generateLadder(height, size);
 
-        assertEquals(5, lines.length);
+        lines.getLines().forEach(line -> {
+            assertEquals(size, line.getLine().length);
+        });
+
+        assertEquals(Integer.parseInt(height.getHeight()), lines.getLines().size());
     }
 }
