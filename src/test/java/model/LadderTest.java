9894ad3d7ac460379cd969b3958df42b41cc2ae3
dongkyunKim <kyun010713@gmail.com>
2024-05-20 13:21:54 +0900

    refactor: 사다리가 정상적으로 생성되는지 테스트 로직 수정

--- src/test/java/model/LadderTest.java
+++ src/test/java/model/LadderTest.java
@@ -1,5 +1,9 @@
 package model;
 
+import static model.ladder.Bridge.EMPTY;
+import static model.ladder.Bridge.EXIST;
+
+import java.util.List;
 import model.ladder.Ladder;
 import model.ladder.strategy.GenerateStrategy;
 import model.ladder.strategy.RandomGenerateStrategy;
@@ -9,27 +13,17 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+
 class LadderTest {
     @Test
-    @DisplayName("정상적으로 사다리 객체 생성")
-    void should_CreateLadderObjectSuccessfully() {
-        // given
-        GenerateStrategy generateStrategy = new RandomGenerateStrategy();
-
-        // when
-        Ladder ladder = new Ladder(generateStrategy);
-
-        // then
-        assertNotNull(ladder);
-    }
-
-    @Test
-    @DisplayName("정상적으로 사다리 초기화")
+    @DisplayName("정상적으로 사다리 생성되는지 확인")
     void should_InitializeLadderSuccessfully() {
         // given
-        GenerateStrategy generateStrategy = new RandomGenerateStrategy();
-        int countOfPlayer = 4;
-        int countOfHeight = 5;
+        GenerateStrategy generateStrategy = new FixedGenerateStrategy(List.of(true, false, true));
+        int countOfPlayer = 3;
+        int countOfHeight = 3;
+        List<List<Boolean>> exceptedResult = List.of(List.of(true, false, true), List.of(true, false, true),
+                List.of(true, false, true));
 
         // when
         Ladder ladder = new Ladder(generateStrategy);
@@ -37,22 +31,6 @@ class LadderTest {
 
         // then
         assertNotNull(ladder);
-        assertEquals(countOfHeight, ladder.getRowSize());
-    }
-
-    @Test
-    @DisplayName("정상적으로 사다리 정보 반환")
-    void should_ReturnLadderInformationSuccessfully() {
-        // given
-        GenerateStrategy generateStrategy = new RandomGenerateStrategy();
-        int countOfPlayer = 4;
-        int countOfHeight = 5;
-
-        // when
-        Ladder ladder = new Ladder(generateStrategy);
-        ladder.init(countOfPlayer, countOfHeight);
-
-        // then
-        assertNotNull(ladder.getLadderInformation());
+        assertEquals(exceptedResult, ladder.getLadder());
     }
 }
