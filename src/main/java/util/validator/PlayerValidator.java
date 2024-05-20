6277db2c1b971320ebdc7d21fa25b762afdb9abf
woogym <kwj9294@gmail.com>
2024-05-20 15:37:32 +0900

    fix: modify PlayerValidator method format

--- src/main/java/util/validator/PlayerValidator.java
+++ src/main/java/util/validator/PlayerValidator.java
@@ -2,6 +2,7 @@ package util.validator;
 
 import exception.LadderGameValidationException;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -15,37 +16,28 @@ public class PlayerValidator {
     }
 
     private static void validatePlayerNameDuplicated(String[] playerNames) {
-        Set<String> carNameSet = new HashSet<>();
-        for (String name : playerNames) {
-            if (!carNameSet.add(name)) {
-                throw new LadderGameValidationException.DuplicatedPlayerNameException();
-            }
+        Set<String> playerNameSet = new HashSet<>();
+        if (Arrays.stream(playerNames).anyMatch(name -> !playerNameSet.add(name))) {
+            throw new LadderGameValidationException.DuplicatedPlayerNameException();
         }
     }
 
     private static void validatePlayerNameIsNotEmpty(String[] playerNames) {
-        for (String name : playerNames) {
-            if (name.isEmpty()) {
-                throw new LadderGameValidationException.NotAllowEmptyInputException();
-            }
+        if (Arrays.stream(playerNames).anyMatch(String::isEmpty)) {
+            throw new LadderGameValidationException.NotAllowEmptyInputException();
         }
     }
 
     private static void validatePlayerNameHasNoSpace(String[] playerNames) {
-        for (String name : playerNames) {
-            if (name.contains(" ")) {
-                throw new LadderGameValidationException.BlankPlayerNameException();
-            }
+        if (Arrays.stream(playerNames).anyMatch(name -> name.contains(" "))) {
+            throw new LadderGameValidationException.BlankPlayerNameException();
         }
     }
 
     private static void validatePlayerNameLength(String[] playNames) {
         final int LENGTH_LIMIT_NUMBER = 5;
-
-        for (String name : playNames) {
-            if (name.length() > LENGTH_LIMIT_NUMBER) {
-                throw new LadderGameValidationException.InvalidPlayerNameLengthException();
-            }
+        if (Arrays.stream(playNames).anyMatch(name -> name.length() > LENGTH_LIMIT_NUMBER)) {
+            throw new LadderGameValidationException.InvalidPlayerNameLengthException();
         }
     }
 }
