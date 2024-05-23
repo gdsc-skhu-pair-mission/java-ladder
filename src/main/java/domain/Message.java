5ebedc9ef867eec1dac8a63f95bbcc1215325e82
Junyeong-An <2117ab@gmail.com>
2024-05-23 18:50:16 +0900

    refactor: Message 공백 추가

--- src/main/java/domain/Message.java
+++ src/main/java/domain/Message.java
@@ -2,7 +2,7 @@ package domain;
 
 public enum Message {
     INPUT_HUMAN_NAME("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)"),
-    INPUT_LADDER_HEIGHT("최대 사다리 높이는 몇 개인가요?"),
+    INPUT_LADDER_HEIGHT(" 최대 사다리 높이는 몇 개인가요?"),
     COLUMN("|"),
     BRIDGE("-----"),
     SPACE(" "),
