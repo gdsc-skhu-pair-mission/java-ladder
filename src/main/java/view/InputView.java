a4875c92e93c9fcbbeceb1d3a57249889062da90
Junyeong-An <2117ab@gmail.com>
2024-05-23 19:26:33 +0900

    refactor: 이름 출력 안내 메시지

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,6 +1,9 @@
 package view;
 
+import domain.Message;
+
 import java.util.Scanner;
+
 
 public class InputView {
     private Scanner scanner;
@@ -10,12 +13,21 @@ public class InputView {
     }
 
     public String[] inputNames() {
+        System.out.println(printNamesMessage());
         String input = scanner.nextLine();
         return input.split(",");
     }
+    public String printNamesMessage() {
+        return Message.INPUT_HUMAN_NAME.toString();
+    }
 
     public int inputHeight() {
+        System.out.println(printHeight());
         return scanner.nextInt();
+    }
+
+    public String printHeight() {
+        return Message.INPUT_LADDER_HEIGHT.toString();
     }
 
     public void closeScanner() {
